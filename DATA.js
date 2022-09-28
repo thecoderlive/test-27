a9b631361296f76a882dd3ea10bac2c24ac38348
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 18:01:51 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "45edfdc2-7381-4a23-a779-0de71b8b6d4a",
+    "home_feed": [
+        {
+            "id": "8e74cd85-9f5d-41dc-9ec7-a372d8ed30fe",
+            "artist_name": "sense",
+            "home_carousel": [
+                {
+                    "id": "ad2685f1-f5c8-4051-8e3a-5bac7a308be9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "911a49e8-d385-43e5-97c0-4b055d6a1f66",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "2dcf8084-57cf-4c00-8b12-91b4f2109340",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/343.png"
+                },
+                {
+                    "id": "0af2aec2-aac1-458f-8a0a-e168cf3e4b85",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "1ceb725a-f9f3-4594-af3b-02d6e0998d36",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                }
+            ],
+            "art_caption": "see",
+            "art_description": "Look any food. According yard sort song whole."
+        },
+        {
+            "id": "c648000e-1e5f-4a7f-9264-0e05885d8dd6",
+            "artist_name": "animal",
+            "home_carousel": [
+                {
+                    "id": "9d272c4a-3bd7-4d33-90f4-dfb93d722763",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png"
+                },
+                {
+                    "id": "682f8ab6-3dcf-491d-8494-09cc4df35154",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                },
+                {
+                    "id": "df46461c-1f2b-4de3-9e5e-081fdb5d7d4f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "5ebf83e0-dcd3-47bf-9483-a666f96152eb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png"
+                },
+                {
+                    "id": "82e49ab0-15bc-4d70-b9c1-a7e3b5fde21f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                }
+            ],
+            "art_caption": "walk",
+            "art_description": "Possible suffer somebody add food. Thought movement building where instead contain. Yeah price nice seek range different own blood."
+        },
+        {
+            "id": "8fdf8ee3-988e-4526-9d96-961bd7e943bb",
+            "artist_name": "shake",
+            "home_carousel": [
+                {
+                    "id": "6981f3e6-33a3-4e77-a41a-8004c857c0a1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                },
+                {
+                    "id": "29481485-ee4f-42ea-8edd-16c41a4ac52a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "ca63fac4-b503-4f15-bc41-2fe8a97ebcfb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png"
+                },
+                {
+                    "id": "e8541d2f-c9fb-4667-92a5-81bcc44e4079",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+                },
+                {
+                    "id": "6d761847-2b61-4664-8f4c-855f167d87f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                }
+            ],
+            "art_caption": "over",
+            "art_description": "Support entire finally. Our be particular close fund. Ok another leader father respond management color treat. Wide summer result during shake."
+        },
+        {
+            "id": "f06cca88-48ca-4174-8791-900043fec166",
+            "artist_name": "stand",
+            "home_carousel": [
+                {
+                    "id": "c4a3cdda-bf06-4fad-a55f-7ae093968809",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png"
+                },
+                {
+                    "id": "89a1ec8d-a4c1-429c-a21a-7da3f28263f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "ccebae2b-d20f-49a6-b4ee-e136734201a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                },
+                {
+                    "id": "da0daca8-2c1f-417c-b60b-682a7c71e6df",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "4ca13da5-77d9-4fe8-abae-d8fe6ad33ff1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/266.png"
+                }
+            ],
+            "art_caption": "industry",
+            "art_description": "Loss some need tough enter baby than. Explain doctor thing degree wind nice design."
+        },
+        {
+            "id": "1e0018c8-1c6a-40f2-960c-508ad2573a96",
+            "artist_name": "stand",
+            "home_carousel": [
+                {
+                    "id": "261ae218-a272-4671-a537-151eb90b40b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "25aa376a-2116-4135-82c6-27a7324d5718",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png"
+                },
+                {
+                    "id": "6f40a1ab-1261-47cc-9c9f-6260c8ebc9f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "8c4fa3d0-7290-4cff-aac1-1da1bea88218",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "0a3b893d-fbab-4621-80df-ad770eddf0d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/217.png"
+                }
+            ],
+            "art_caption": "imagine",
+            "art_description": "Issue admit arrive this establish kid spring. One skin age support possible since. Including them wait color."
+        }
+    ]
+}
